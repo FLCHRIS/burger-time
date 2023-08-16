c0d090ebc04018848dc48e1ec172bfc86e241733
Christian <filetelopezchristian@gmail.com>
2023-08-15 20:59:47 -0500

    Se agregó el componente Home

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Layout } from '../layout/Layout'
+import { Home } from '../pages/Home'
 
 export const Router: React.FC = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<h1>Home</h1>} />
+        <Route index element={<Home />} />
         <Route path='menu' element={<h1>Menu</h1>} />
         <Route path='service' element={<h1>Services</h1>} />
         <Route path='about' element={<h1>About Us</h1>} />
