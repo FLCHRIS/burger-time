77320adb50eca5a01a0dc3021f250dff838c322d
Christian <filetelopezchristian@gmail.com>
2023-08-16 20:09:32 -0500

    Se agregó un evento al botón de agregar producto

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import { type IProduct } from '../interface/types'
+import React, { useContext } from 'react'
+import { type IProduct, type IProductCart } from '../interface/types'
 import { BiCartAdd } from 'react-icons/bi'
 import Atropos from 'atropos/react'
+import { UserContext } from '../context/UserContext'
 
 export const Card: React.FC<IProduct> = ({ id, image, name, price, information }) => {
+  const cart = useContext(UserContext)
+
+  const handleAddProduct = (): void => {
+    const newProduct: IProductCart = {
+      id,
+      image,
+      name,
+      price,
+      information,
+      quantity: 1
+    }
+    cart?.addCart(newProduct)
+  }
+
   return (
     <div className='card'>
       <Atropos
@@ -14,8 +29,8 @@ export const Card: React.FC<IProduct> = ({ id, image, name, price, information }
       <h3 className='card__name'>{name}</h3>
       <p className='card__info'>{information}</p>
       <div className='card__container'>
-        <span className='card__container__price'>$ {price}</span>
-        <button aria-label='Add to cart' className='card__container__button'>
+        <span className='card__container__price' onClick={() => { console.log(cart?.cart) }}>$ {price}</span>
+        <button aria-label='Add to cart' className='card__container__button' onClick={handleAddProduct}>
           <BiCartAdd aria-hidden='true' />
         </button>
       </div>
