e6d6c9c960a253f2c95311b374d033bcf89ad0e2
Christian <filetelopezchristian@gmail.com>
2023-08-17 12:24:40 -0500

    Se agregó el componentes Footer.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Footer } from './Footer'
 import { Header } from './Header'
 
 export const Layout: React.FC = () => {
@@ -9,6 +10,7 @@ export const Layout: React.FC = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   )
 }
