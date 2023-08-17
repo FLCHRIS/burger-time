988aef70c0352ce64461ee769f38de6274c48909
Christian <filetelopezchristian@gmail.com>
2023-08-16 20:10:36 -0500

    Se agregaron interface y types

--- src/interface/types.d.ts
+++ src/interface/types.d.ts
@@ -5,3 +5,25 @@ export interface IProduct {
   price: number
   information: string
 }
+
+export interface IProductCart extends IProduct {
+  id: number
+  name: string
+  image: string
+  price: number
+  information: string
+  quantity: number
+}
+
+export interface ICart {
+  total: number
+  subtotal: number
+  products: IProductCart[]
+}
+
+export interface IUserContext {
+  cart: ICart
+  addCart: (product: IProductCart) => void
+}
+
+export type UserContextType = IUserContext | null
